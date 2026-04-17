39eb389ad0a1d7c51cbae197337e5f8311e35baa
bill <bill@billdeMac-mini.local>
2026-04-17 20:28:35 +0900

    feat: Add base path for GitHub Pages in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/roogoo-h5-app/',
   plugins: [
     vue(),
     vueDevTools(),
